56e44fb096315523e9c021530144f00013c50e0a
eferos93 <eros.fabrici@gmail.com>
2021-03-12 12:06:58 +0100

    added template for help

--- second_app/urls.py
+++ second_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from second_app import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^help/', views.help_http, name='help')
 ]
--- second_app/views.py
+++ second_app/views.py
@@ -6,3 +6,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+
+def help_http(request):
+    help_dictionary = {'help_inserts': 'Help Page'}
+    return render(request, 'second_app/help.html', help_dictionary)
