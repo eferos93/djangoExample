b5a00a283b74e0f81b86ca20881ce218fa595138
eferos93 <eros.fabrici@gmail.com>
2021-03-12 15:17:33 +0100

    added static file (img)

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR / 'templates')  # use it to call our new templates directory
+# print(BASE_DIR / 'templates')  # use it to call our new templates directory
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    BASE_DIR / 'static',
+]
+print(BASE_DIR / 'static')
