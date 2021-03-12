7d01432c2921f968d8e8744fe21dc5f36ee4c3f1
eferos93 <eros.fabrici@gmail.com>
2021-03-12 15:43:54 +0100

    app set up!

--- models/first_model_app/views.py
+++ models/first_model_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    my_dictionary = { 'insert_content': 'Hello I am from first_model_app'}
+    return render(request, 'first_model_app/index.html', context=my_dictionary)
