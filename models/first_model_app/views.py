7096131b7f2997a2bae896eecc167c1cf34c8220
eferos93 <eros.fabrici@gmail.com>
2021-03-15 17:52:36 +0100

    full example of MVT in action

--- models/first_model_app/views.py
+++ models/first_model_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from first_model_app.models import Topic, Webpage, AccessRecord
+
 
 # Create your views here.
 
 def index(request):
-    my_dictionary = { 'insert_content': 'Hello I am from first_model_app'}
-    return render(request, 'first_model_app/index.html', context=my_dictionary)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dictionary = {'access_records': webpages_list}
+    return render(request, 'first_model_app/index.html', context=date_dictionary)
