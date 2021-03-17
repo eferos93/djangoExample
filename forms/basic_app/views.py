90d5215b594a3bcc403d3f28b465696375085bd8
eferos93 <eros.fabrici@gmail.com>
2021-03-17 17:21:15 +0100

    forms and view setted up

--- forms/basic_app/views.py
+++ forms/basic_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from . import forms
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'basic_app/index.html')
+
+
+def form_name_view(request):
+    form = forms.FormName()
+    return render(request, 'basic_app/form.html', {'form': form})
