de79c64d4009ac9b1402921330d67f99cfc5ceeb
eferos93 <eros.fabrici@gmail.com>
2021-03-17 18:33:16 +0100

    more validator examples

--- forms/basic_app/forms.py
+++ forms/basic_app/forms.py
@@ -9,13 +9,23 @@ def check_for_z(value):
 
 
 class FormName(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    # name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
-    bot_catcher = forms.CharField(required=False,
-                                  widget=forms.HiddenInput,
-                                  validators=[validators.MaxLengthValidator(0)])
+    # bot_catcher = forms.CharField(required=False,
+    #                               widget=forms.HiddenInput,
+    #                               validators=[validators.MaxLengthValidator(0)])
 
+    def clean(self):
+        all_clean_data = super().clean() # cleans all data
+        # now do custom stuff
+        email = all_clean_data['email']
+        v_email = all_clean_data['verify_email']
+
+        if email != v_email:
+            raise forms.ValidationError('Make sure emails match')
     # # this method is overridden
     # Manually validate form field
     # def clean_bot_catcher(self):
