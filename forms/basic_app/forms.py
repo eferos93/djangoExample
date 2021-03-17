9e30c3c4bee9ee3959a06a94551fceffce46fe32
eferos93 <eros.fabrici@gmail.com>
2021-03-17 18:27:45 +0100

    more validator examples

--- forms/basic_app/forms.py
+++ forms/basic_app/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from django.core import validators
 
+
+# custom validation function: the argument name must be be 'value'
+def check_for_z(value):
+    if value[0].lower != 'z':
+        raise forms.ValidationError("Name needs to start with z")
+
+
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
     bot_catcher = forms.CharField(required=False,
