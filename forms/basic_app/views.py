021d79f08b7809c20f8e4ab4aa18a92f2fd94559
eferos93 <eros.fabrici@gmail.com>
2021-03-17 17:40:28 +0100

    first form

--- forms/basic_app/views.py
+++ forms/basic_app/views.py
@@ -9,4 +9,13 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print('validation success')
+            print('Name: ' + form.cleaned_data['name'])
+            print('Email: ' + form.cleaned_data['email'])
+            print('Text: ' + form.cleaned_data['text'])
+
     return render(request, 'basic_app/form.html', {'form': form})
