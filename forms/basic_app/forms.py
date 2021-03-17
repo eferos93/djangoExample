178472248d57e04fb75ee6d8f0d9d17b63602cf2
eferos93 <eros.fabrici@gmail.com>
2021-03-17 18:21:14 +0100

    added validator

--- forms/basic_app/forms.py
+++ forms/basic_app/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    bot_catcher = forms.CharField(required=False,
+                                  widget=forms.HiddenInput,
+                                  validators=[validators.MaxLengthValidator(0)])
+
+    # # this method is overridden
+    # Manually validate form field
+    # def clean_bot_catcher(self):
+    #     bot_catcher = self.cleaned_data['bot_catcher']
+    #     if len(bot_catcher) > 0:
+    #         raise forms.ValidationError('GOTCHA BOT!')
+    #     return bot_catcher
