780aa3ee3a1b6957e2c0bff9e350064e16ba3204
eferos93 <eros.fabrici@gmail.com>
2021-03-18 14:29:06 +0100

    added comment

--- forms/basic_app/forms.py
+++ forms/basic_app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core import validators
 
 
+# CREATE A FORM ------------------------------------------------------
 # custom validation function: the argument name must be be 'value'
 def check_for_z(value):
     if value[0].lower != 'z':
@@ -14,12 +15,13 @@ class FormName(forms.Form):
     email = forms.EmailField()
     verify_email = forms.EmailField(label='Enter your email again:')
     text = forms.CharField(widget=forms.Textarea)
+
     # bot_catcher = forms.CharField(required=False,
     #                               widget=forms.HiddenInput,
     #                               validators=[validators.MaxLengthValidator(0)])
 
     def clean(self):
-        all_clean_data = super().clean() # cleans all data
+        all_clean_data = super().clean()  # cleans all data
         # now do custom stuff
         email = all_clean_data['email']
         v_email = all_clean_data['verify_email']
@@ -33,3 +35,6 @@ class FormName(forms.Form):
     #     if len(bot_catcher) > 0:
     #         raise forms.ValidationError('GOTCHA BOT!')
     #     return bot_catcher
+# --------------------------------------------------------------------------------
+
+# CREATE A FORM FROM AN EXISTING MODEL
