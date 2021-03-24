a6264c5cf95e9310edfb14f4aa60b18e1abf0b51
Eros Fabrici <eros.fabrici@gmail.com>
2021-03-24 10:40:23 +0100

    added views.py

--- learningUsers/basic_app/form.py
+++ learningUsers/basic_app/form.py
@@ -11,7 +11,7 @@ class UserForm(forms.ModelForm):
         fields = ('username', 'email', 'password')
 
 
-class UserProfile(forms.ModelForm):
+class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfileInfo
         fields = ('portfolio_site', 'profile_pic')
