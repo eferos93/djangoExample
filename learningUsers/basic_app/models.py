b65e48569c3164def10caf73fe0f52bd2064b4e0
Eros Fabrici <eros.fabrici@gmail.com>
2021-03-24 09:58:44 +0100

    models and forms setted up

--- learningUsers/basic_app/models.py
+++ learningUsers/basic_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+    # custom fields
+    portfolio_site = models.URLField(blank=True)
+    # uploaded images are gonna be saved in media/profile_pics
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
