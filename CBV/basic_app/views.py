c584fc13aeddca1d1114bdc18b63631d01a06e32
Eros Fabrici <eros.fabrici@gmail.com>
2021-03-29 16:56:04 +0200

    injection with template view

--- CBV/basic_app/views.py
+++ CBV/basic_app/views.py
@@ -10,3 +10,8 @@ from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = 'basic_app/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['inject_me'] = 'Basic injection'
+        return context
