8d3e3934df93402ce4b6f6abb499bfb36940f62e
eferos93 <eros.fabrici@gmail.com>
2021-04-06 14:55:45 +0200

    added baisc_app templates; added DetailView and ListView CBV

--- CBV/basic_app/views.py
+++ CBV/basic_app/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
 from django.http import HttpResponse
+from . import models
 # Create your views here.
 
 
 # class CBView(View):
 #     def get(self, request):
 #         return HttpResponse('Class Based Views are cool')
+#
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['inject_me'] = 'Basic injection'
+#         return context
 
-class IndexView(TemplateView):
-    template_name = 'basic_app/index.html'
+class SchoolListView(ListView):
+    model = models.School
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['inject_me'] = 'Basic injection'
-        return context
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
