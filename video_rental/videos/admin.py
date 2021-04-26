5c7cfe1bca2f9703b57684c7d7f0ffedf2aaa49d
Eros Fabrici <eros.fabrici@gmail.com>
2021-04-26 16:56:48 +0200

    added search fields in admin site

--- video_rental/videos/admin.py
+++ video_rental/videos/admin.py
@@ -7,6 +7,8 @@ from . import models
 class MovieAdmin(admin.ModelAdmin):
     # order of the fields when showed in detail view inside admin site
     fields = ['release_year', 'title', 'length']
+    # django will create a search box for title
+    search_fields = ['title']
 
 
 admin.site.register(models.Customer)
