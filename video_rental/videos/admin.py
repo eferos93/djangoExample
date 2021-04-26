afe718dcca5f87addfbb8c8efbdb63426ea50ebc
Eros Fabrici <eros.fabrici@gmail.com>
2021-04-26 16:54:51 +0200

    change field order in detail view admin site

--- video_rental/videos/admin.py
+++ video_rental/videos/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from . import models
+
+
 # Register your models here.
 
+class MovieAdmin(admin.ModelAdmin):
+    # order of the fields when showed in detail view inside admin site
+    fields = ['release_year', 'title', 'length']
+
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie, MovieAdmin)
